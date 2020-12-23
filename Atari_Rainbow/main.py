c2547fc4f1e2af76cc2121c0601c455776adca38
tobyj2 <johnstone.toby@gmail.com>
2020-12-23 12:06:02 +0100

    new

--- Atari_Rainbow/main.py
+++ Atari_Rainbow/main.py
@@ -58,7 +58,7 @@ parser.add_argument('--enable-cudnn', action='store_true', help='Enable cuDNN (f
 parser.add_argument('--checkpoint-interval', default=0, help='How often to checkpoint the model, defaults to 0 (never checkpoint)')
 parser.add_argument('--memory', help='Path to save/load the memory from')
 parser.add_argument('--disable-bzip-memory', action='store_true', help='Don\'t zip the memory file. Not recommended (zipping is a bit slower and much, much smaller)')
-parser.add_argument('--use-TPU', action='store_true')
+parser.add_argument('--use-TPU', action='store_false')
 
 
 # Setup
